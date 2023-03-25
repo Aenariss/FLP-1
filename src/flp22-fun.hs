a30e61d9905f61c315a3386d54cf0ce56818cdc3
aenariss <aenariss@gmail.com>
2023-03-25 15:35:49 +0100

    rewrite internet functions w/ my own implementation derived from python equivalents

--- src/flp22-fun.hs
+++ src/flp22-fun.hs
@@ -5,7 +5,6 @@ Author: Vojtech Fiala <xfiala61>
 -}
 
 import qualified Types
-import qualified Data.Char as D
 import qualified ParseStructures as PS
 import qualified System.IO as I
 import qualified System.Environment as E
@@ -38,15 +37,18 @@ printHelp = do
 -- because of Haskell and its weird IO stuff, this will keep producing the same number because I cant do anything else
 getRandomInInterval :: Integer -> Integer
 getRandomInInterval top =
-    fst(Random.uniformR(1 :: Integer, (top-1) :: Integer) gen)
-    where gen = Random.mkStdGen 42
+    fst(Random.uniformR(1, (top-1)) gen)
+    where 
+        gen = Random.mkStdGen 42
 
 -- function to convert integer to its binary representation in a string and reverse it
 binary :: Integer -> String
 binary x = reverse (binaryHelp x)
-    where binaryHelp 0 = "0" 
-          binaryHelp 1 = "1"
-          binaryHelp a = binaryHelp (a `div` 2) ++ show (a `mod` 2)
+    where 
+        binaryHelp :: Integer -> String
+        binaryHelp 0 = "0" 
+        binaryHelp 1 = "1"
+        binaryHelp a = binaryHelp (a `div` 2) ++ show (a `mod` 2)
 
 -- https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication#Double-and-add
 -- function to calculate point multiplication using double and add algo
@@ -56,29 +58,24 @@ doubleAndAdd tmpPoint resultPoint a p privateStr
     | otherwise = if head privateStr == '1' 
                     then doubleAndAdd newTemp newRes a p newStr 
                     else doubleAndAdd newTemp resultPoint a p newStr
-    where newTemp = pointAdd tmpPoint tmpPoint a p
-          newRes = pointAdd resultPoint tmpPoint a p
-          newStr = tail privateStr
-
-{- -- version with foldr, looks better but doesnt work (perhaps fix later?)
-doubleAndAdd :: Types.Point -> Types.Point -> Integer -> Integer -> String -> Types.Point
-doubleAndAdd tmpPoint resultPoint a p privateStr =
-  snd(foldr f (tmpPoint, resultPoint) privateStr)
-  where
-        f :: Char -> (Types.Point, Types.Point) -> (Types.Point, Types.Point)
-        f '1' (tmpPoint, resultPoint) = ((pointAdd tmpPoint tmpPoint a p), (pointAdd resultPoint tmpPoint a p))
-        f '0' (tmpPoint, resultPoint) = ((pointAdd tmpPoint tmpPoint a p), resultPoint)
--}
+    where 
+        newTemp = pointAdd tmpPoint tmpPoint a p
+        newRes = pointAdd resultPoint tmpPoint a p
+        newStr = tail privateStr
 
 -- function to simulate python-like pow(x,y,z)
--- https://www.reddit.com/r/haskell/comments/mqtk6/comment/c33n70a/?utm_source=share&utm_medium=web2x&context=3
 -- incredibly more effective than x^y % p
-fastPow :: Integer -> Integer -> Integer -> Integer
-fastPow base exp' modulo = fastpow' (base `mod` modulo) exp' modulo 1
-    where fastpow' _ 0 _ r = r
-          fastpow' b e m r = fastpow' (b * b `mod` m) (e `div` 2) m (if even e then r else (r * b `mod` m))
+-- https://www.geeksforgeeks.org/exponential-squaring-fast-modulo-multiplication/ -based on python ver
+pow :: Integer -> Integer -> Integer -> Integer
+pow base exp' modul 
+    | exp' == 0 = 1
+    | exp' == 1 = base `mod` modul
+    | (exp' `mod` 2 == 0) = t
+    | otherwise = ((base `mod` modul) * t) `mod` modul
+    where 
+        t = (pow base (exp' `div` 2) modul)^(2::Integer) `mod` modul
 
--- https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
+-- https://stackoverflow.com/questions/31074172/elliptic-curve-point-addition-over-a-finite-field-in-python
 -- function to add 2 points
 pointAdd :: Types.Point -> Types.Point -> Integer -> Integer -> Types.Point
 pointAdd (Types.Point xp yp) (Types.Point xq yq) a p
@@ -87,72 +84,85 @@ pointAdd (Types.Point xp yp) (Types.Point xq yq) a p
     | (xp == xq && yq == yp) = if yp == 0 then Types.Point 0 0 else Types.Point xr1 ((m1*(xp-xr1)-yp) `mod` p)
     | xp == xq = Types.Point 0 0
     | otherwise = Types.Point xr2 ((m2*(xp-xr2)-yp) `mod` p)
-    where m1 = mSame xp a yp p
-          xr1 = (m1^(2::Integer) - xp - xq) `mod` p
-          m2 = mDiff xp yp xq yq p
-          xr2 = (m2^(2::Integer) - xp - xq) `mod` p
-
-mDiff :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
-mDiff xp yp xq yq p = (((yq - yp) * (fastPow (xq - xp) (p-2) p)) `mod` p)
-
-mSame :: Integer -> Integer -> Integer -> Integer -> Integer
-mSame xp a yp p = (((3*xp*xp + a) * (fastPow (2*yp) (p-2) p)) `mod` p)
+    where 
+        m1 = ((3*xp*xp + a) * (pow (2*yp) (p-2) p)) `mod` p
+        xr1 = (m1^(2::Integer) - xp - xq) `mod` p
+        m2 = ((yq - yp) * (pow (xq - xp) (p-2) p)) `mod` p
+        xr2 = (m2^(2::Integer) - xp - xq) `mod` p
 
 -- function to convert an integer to its byte representation, aka 1024 = 0x0400
--- https://www.tutorialspoint.com/haskell-program-to-convert-decimal-to-hexadecimal
 decToHex :: Integer -> String
-decToHex 0 = "0"
-decToHex n = reverse (hexChars n)
-   where
-      hexChars :: Integer -> String
-      hexChars 0 = ""
-      hexChars x = D.intToDigit (fromInteger (x `mod` 16)) : hexChars (x `div` 16)
+decToHex x = hexHelp x
+    where 
+        hexHelp :: Integer -> String
+        hexHelp 0 = "0"
+        hexHelp 1 = "1"
+        hexHelp 2 = "2"
+        hexHelp 3 = "3"
+        hexHelp 4 = "4"
+        hexHelp 5 = "5"
+        hexHelp 6 = "6"
+        hexHelp 7 = "7"
+        hexHelp 8 = "8"
+        hexHelp 9 = "9"
+        hexHelp 10 = "a"
+        hexHelp 11 = "b"
+        hexHelp 12 = "c"
+        hexHelp 13 = "d"
+        hexHelp 14 = "e"
+        hexHelp 15 = "f"
+        hexHelp n = hexHelp (n `div` 16) ++ hexHelp (n `mod` 16)
 
 -- function to calculate private & public key pair from given eliptic Curve
 calculateKey :: Types.Curve -> Types.Key
 calculateKey (Types.Curve p a _ g n _) =
     Types.Key randomPrivate public
-    where randomPrivate = getRandomInInterval (n-1)
-          public = getPublic(doubleAndAdd g (Types.Point 0 0) a p (binary randomPrivate))
+    where 
+        randomPrivate = getRandomInInterval (n-1) -- "randomly" choose an integer in interval
+        public = getPublic(doubleAndAdd g (Types.Point 0 0) a p (binary randomPrivate))
 
 -- function to create the public key from the given X and Y coords
 getPublic :: Types.Point -> String
 getPublic (Types.Point x y) 
     | (y > 0) = if length(decX ++ decY) `mod` 2 == 1 then "0x04" ++ "0" ++ decX ++ decY else "0x04" ++ decX ++ decY
     | otherwise = if length(decX ++ decToHex(-y)) `mod` 2 == 1 then "0x04" ++ "0" ++ decX ++ decToHex(-y) else "0x04" ++ decX ++ decToHex(-y)
-    where decY = decToHex (y)
-          decX = decToHex (x)
+    where 
+        decY = decToHex (y)
+        decX = decToHex (x)
 
 -- function to calculate the X parameter in the signature
 getSignatureX :: Types.Curve -> Integer -> Integer
 getSignatureX (Types.Curve p a _ g n _) k = getX(doubleAndAdd g (Types.Point 0 0) a p (binary k))
-        where getX (Types.Point x y) = x `mod` n
+    where 
+        getX (Types.Point x y) = x `mod` n
 
 -- function to calculate the X parameter in the signature
 getSignatureY :: Types.Curve -> Types.Key -> Types.Hash -> Integer -> Integer -> Integer
 getSignatureY (Types.Curve p a b g n h) (Types.Key priv pub) (Types.Hash hash) r k = 0
 
--- function to calculate modularInverse
--- python code which was linked on the forum doesnot understand that k^-1 doesnt m,ean 1/k ion this case but multiplicate inverse of k
--- shamelessly taken from https://byorgey.wordpress.com/2020/02/15/competitive-programming-in-haskell-modular-arithmetic-part-1/
+-- function to calculate modular multiplicartive inverse
+-- https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
 modularInverse :: Integer -> Integer -> Integer
-modularInverse x y = z `mod` x
-        where (_,_,z) = egcd y x
-
--- shamelessly taken from https://byorgey.wordpress.com/2020/02/15/competitive-programming-in-haskell-modular-arithmetic-part-1/
-egcd :: Integer -> Integer -> (Integer, Integer, Integer)
-egcd a 0 = (abs a, signum a, 0)
-egcd a b = (g, y, x - (a `div` b) * y)
+modularInverse a m = x `mod` m
     where
-        (g,x,y) = egcd b (a `mod` b)
+        (_, x, _) = egcd a m
 
+-- function to calculate modular multiplicartive inverse
+-- https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
+egcd :: Integer -> Integer -> (Integer, Integer, Integer)
+egcd a b 
+    | a == 0 = (b, 0, 1)
+    | otherwise = (g, (x - (b `div` a) * y), y)
+    where (g, y, x) = egcd (b `mod` a) a
+        
 -- function to calculate signature
 calculateSignature :: Types.Sstruct -> Types.Signature
 calculateSignature (Types.Sstruct (Types.Curve p a b g n h) key hash) =
     Types.Signature r s
-    where k = getRandomInInterval (n-1)
-          r = getSignatureX (Types.Curve p a b g n h) k
-          s = getSignatureY (Types.Curve p a b g n h) key hash r (modularInverse k n)
+    where 
+        k = getRandomInInterval (n-1)
+        r = getSignatureX (Types.Curve p a b g n h) k
+        s = getSignatureY (Types.Curve p a b g n h) key hash r (modularInverse k n)
 
 
 -- function to do get the appropriate output depending on the given argument
@@ -167,7 +177,8 @@ actOnParameter args input
         printHelp
         putStrLn "\nUnknown parameter!"
         Exit.exitWith (Exit.ExitFailure 1)
-    where arg = head args
+    where 
+        arg = head args
 
 -- function to parse the arguments that calls the correct function based on the parameter
 parseArgs :: [String] -> IO () 
